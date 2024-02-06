2fe583db0d07add013bb6b8b2d27372dd8585b46
Matheus <112503237+MatthewsVieira@users.noreply.github.com>
2024-02-06 11:51:31 -0300

    Exercicio 4 Concluido

--- untitled/src/semana_04/Main.java
+++ untitled/src/semana_04/Main.java
@@ -9,10 +9,16 @@ public class Main {
 
         ArrayList<String> cursos = new ArrayList<String>();
         ArrayList<String> professores = new ArrayList<String>();
+        ArrayList<String> cursosConcluidos = new ArrayList<String>();
+        ArrayList<String> professorConcluidos = new ArrayList<String>();
+
         boolean bool = true;
 
         iniciarCursos(cursos);
         iniciarProfessores(professores);
+
+
+
 
         do {
             System.out.println();
@@ -20,6 +26,8 @@ public class Main {
             System.out.println("1 - LISTAR");
             System.out.println("2 - ADICIONAR");
             System.out.println("3 - REMOVER");
+            System.out.println("4 - MARCAR CURSOS CONCLUÍDOS");
+            System.out.println("5 - LISTAR CURSOS CONCLUÍDOS");
             System.out.println("0 - SAIR");
             System.out.println();
 
@@ -30,97 +38,36 @@ public class Main {
 
             switch(opcao) {
                 case 1:
-                    loop = true;
-                    while(loop) {
-                        System.out.println("-----LISTAR-----");
-                        System.out.println("1 - CURSOS");
-                        System.out.println("2 - PROFESSORES");
-                        System.out.print("DIGITE A OPÇÃO DESEJADA: ");
-                        opcao = sc.nextInt();
-                        System.out.println();
-                        switch(opcao){
-                            case 1:
-                                System.out.println("CURSOS: ");
-                                listar(cursos);
-                                loop = false;
-                                break;
-                            case 2:
-                                System.out.println("PROFESSORES: ");
-                                listar(professores);
-                                loop = false;
-                                break;
-                            default:
-                                System.out.println("*DIGITE UMA OPÇÃO VÁLIDA*");
-                                System.out.println();
-                                break;
-                        }
-                    }
+                    System.out.println("-----LISTAR-----");
+                    listar(cursos, professores);
                     break;
                 case 2:
-                    loop = true;
-                    while(loop) {
                         System.out.println("-----ADICIONAR-----");
-                        System.out.println("1 - CURSOS");
-                        System.out.println("2 - PROFESSORES");
-                        System.out.print("DIGITE A OPÇÃO DESEJADA: ");
-                        opcao = sc.nextInt();
                         System.out.println();
-                        switch(opcao){
-                            case 1:
-                                System.out.println("CURSOS: ");
-                                System.out.print("INSIRA O CURSO: ");
-                                adicionar(sc, cursos);
-                                loop = false;
-                                break;
-                            case 2:
-                                System.out.println("PROFESSORES: ");
-                                System.out.print("INSIRA O PROFESSOR: ");
-                                adicionar(sc, professores);
-                                loop = false;
-                                break;
-                            default:
-                                System.out.println("*DIGITE UMA OPÇÃO VÁLIDA*");
-                                System.out.println();
-                                break;
-                        }
-                    }
+                        adicionar(sc, cursos, professores);
                     break;
                 case 3:
-                    loop = true;
-                    while(loop) {
                         System.out.println("-----REMOVER-----");
-                        System.out.println("1 - CURSOS");
-                        System.out.println("2 - PROFESSORES");
-                        System.out.print("DIGITE A OPÇÃO DESEJADA: ");
-                        opcao = sc.nextInt();
                         System.out.println();
-                        switch(opcao){
-                            case 1:
-                                System.out.println("CURSOS: ");
-                                listar(cursos);
-                                System.out.println();
-                                removerItemLista(sc, cursos);
-                                loop = false;
-                                break;
-                            case 2:
-                                System.out.println("PROFESSORES: ");
-                                listar(professores);
-                                System.out.println();
-                                removerItemLista(sc, professores);
-                                loop = false;
-                                break;
-                            default:
-                                System.out.println("*DIGITE UMA OPÇÃO VÁLIDA*");
-                                System.out.println();
-                                break;
-                        }
-                    }
+                        listar(cursos, professores);
+                        System.out.println();
+                        removerItemLista(sc, cursos, professores);
+
+                    break;
+                case 4:
+                    System.out.println("----MARCAR CURSOS CONCLUIDOS----");
+                    System.out.println();
+                    marcarConcluido(cursosConcluidos, cursos, professorConcluidos, professores ,sc);
+                    break;
+                case 5:
+                    System.out.println("----CURSOS CONCLUIDOS----");
+                    listar(cursosConcluidos, professorConcluidos);
                     break;
                 case 0:
                     System.out.println("SAINDO...");
                     bool = false;
                     break;
-            }
+           }
 
         } while(bool);
 
@@ -142,25 +89,31 @@ public class Main {
         lista.add("Sandra");
     }
 
-    public static void adicionar(Scanner sc, ArrayList<String> lista) {
+    public static void adicionar(Scanner sc, ArrayList<String> curso, ArrayList<String> professor) {
+        System.out.print("INSIRA O CURSO: ");
         sc.nextLine();
         String nome = sc.nextLine();
-        lista.add(nome);
+        curso.add(nome);
+        System.out.print("INSIRA O PROFESSOR RESPONSÁVEL: ");
+        nome = sc.nextLine();
+        professor.add(nome);
     }
 
-    public static void listar(ArrayList<String> lista) {
-        for (int i = 0; i < lista.size(); i++) {
-            System.out.println(i + " - " + lista.get(i));
+    public static void listar(ArrayList<String> curso, ArrayList<String> professor) {
+        for (int i = 0; i < curso.size(); i++) {
+            System.out.println(i + " - CURSO: " + curso.get(i) + " - PROFESSOR: " + professor.get(i));
         }
     }
 
-    public static void removerItemLista(Scanner sc, ArrayList<String> lista){
-        removerItem(lista, listarPedirIndice(sc));
+    public static void removerItemLista(Scanner sc, ArrayList<String> curso, ArrayList<String> professor){
+        System.out.print("DIGITE O NÚMERO CORRESPONDENTE PARA REMOÇÃO: ");
+        int indice = listarPedirIndice(sc);
+        removerItem(curso, indice);
+        removerItem(professor, indice);
     }
 
 
     public static int listarPedirIndice(Scanner sc) {
-        System.out.print("ESCOLHA UM INDICE DA LISTA PARA REMOÇÃO: ");
         int indice = sc.nextInt();
         return indice;
     }
@@ -168,6 +121,16 @@ public class Main {
     public static void removerItem(ArrayList<String> lista, int indice) {
         lista.remove(indice);
     }
+
+    public static void marcarConcluido(ArrayList<String> cursosConcluidos, ArrayList<String> curso, ArrayList<String> professoresConcluidos, ArrayList<String> professores,Scanner sc) {
+        System.out.print("DIGITE O NÚMERO CORRESPONDENTE AO INDICE DO CURSO CONCLUIDO: ");
+        int indice = listarPedirIndice(sc);
+        cursosConcluidos.add(curso.get(indice));
+        removerItem(curso, indice);
+        professoresConcluidos.add(professores.get(indice));
+        removerItem(professores, indice);
+    }
+
 
 
 
