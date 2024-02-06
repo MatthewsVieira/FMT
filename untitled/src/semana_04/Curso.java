3209adbcace5023f7d145ce5acfcd3872b243360
Matheus <112503237+MatthewsVieira@users.noreply.github.com>
2024-02-06 11:56:23 -0300

    Exercicio 5 Concluido

---
+++ untitled/src/semana_04/Curso.java
@@ -0,0 +1,23 @@
+package semana_04;
+
+import java.util.ArrayList;
+
+public class Curso {
+    public String nome;
+    public String descricao;
+    public ArrayList<String> professores;
+    public int cargaHoraria;
+
+    public void progresso() {
+        System.out.println("Progredindo...");
+    }
+
+    public void notas() {
+        System.out.println("Avaliando...");
+    }
+
+    public void participacao {
+        System.out.println("Participando...");
+    }
+
+}
---
+++ untitled/src/semana_04/Professor.java
@@ -0,0 +1,11 @@
+package semana_04;
+
+public class Professor {
+    public String nome;
+    public String especilidade;
+
+    public void ensinar() {
+        System.out.println("Ensinando...");
+    }
+
+}
