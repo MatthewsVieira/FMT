1e2d9425c6747043ff8c8d1049f26bd129dc1992
Matheus <112503237+MatthewsVieira@users.noreply.github.com>
2024-02-08 10:31:18 -0300

    Exercicio 2 - Semana 5 - Concluido

--- untitled/src/semana_05/Jogador.java
+++ untitled/src/semana_05/Jogador.java
@@ -6,6 +6,28 @@ public class Jogador {
     private int pontuacao;
     private int numeroTentativas;
 
+    public Jogador(String nome, int idade, int pontuacao, int numeroTentativas) {
+        this.nome = nome;
+        this.idade = idade;
+        this.pontuacao = pontuacao;
+        this.numeroTentativas = numeroTentativas;
+    }
+
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getNome() { return nome; }
+
+    public void setIdade(int idade) { this.idade = idade; }
+
+    public int getIdade() { return idade; }
+
+    public void setPontuacao(int pontuacao) { this.pontuacao = pontuacao; }
+
+    public int getPontuacao() { return pontuacao; }
+
+    public void setNumeroTentativas(int numeroTentativas) { this.numeroTentativas = numeroTentativas; }
+
+    public int getNumeroTentativas() { return numeroTentativas; }
 
     public void adicionaPontos() {
 
