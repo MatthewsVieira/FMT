f8a864303cdd090f8e7a815a155043a4dd4c5b44
Matheus <112503237+MatthewsVieira@users.noreply.github.com>
2024-02-08 16:52:25 -0300

    Exercicio 4 - Semana 5

--- untitled/src/semana_05/Main.java
+++ untitled/src/semana_05/Main.java
@@ -2,32 +2,38 @@ package semana_05;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
 
-        Jogador jogador1 = new Jogador("Abner", 18, 200, 5);
-        Jogador jogador2 = new Jogador("Vanderlei", 24, 500, 2);
-        Jogador jogador3 = new Jogador("Lucas", 15, 180, 3);
-        Jogador jogador4 = new Jogador("Amanda", 27, 554, 7);
-        Jogador jogador5 = new Jogador("Tyler", 19, 50, 1);
-
         ArrayList<Jogador> ranking = new ArrayList<>();
-
-        ranking.add(jogador1);
-        ranking.add(jogador2);
-        ranking.add(jogador3);
-        ranking.add(jogador4);
-        ranking.add(jogador5);
-
+        inicializarJogadores(ranking);
         Collections.sort(ranking);
-
-
-
-        for (int i = 0; i < ranking.size(); i++) {
-            System.out.println((i + 1) + "º Lugar: " + ranking.get(i).getNome() + " - Pontuação: " + ranking.get(i).getPontuacao());
-        }
-
+        imprimirRanking(ranking);
     }
-}
+    public static void inicializarJogadores(ArrayList<Jogador> ranking){
+        ranking.add(new Jogador ("Abner", 18, 200, 5));
+        ranking.add(new Jogador ("Vanderlei", 24, 500, 2));
+        ranking.add(new Jogador ("Lucas", 15, 180, 3));
+        ranking.add(new Jogador ("Amanda", 27, 554, 7));
+        ranking.add(new Jogador ("Tyler", 19, 50, 1));
+        ranking.add(new Jogador ("Mot", 19, 60, 1));
+        ranking.add(new Jogador ("Fat", 19, 80, 1));
+        ranking.add(new Jogador ("Thin", 19, 70, 1));
+        ranking.add(new Jogador ("Young", 19, 10, 1));
+        ranking.add(new Jogador ("Old", 19, 93, 1));
+        ranking.add(new Jogador ("New", 19, 88, 1));
+    }
+    public static void imprimirRanking(ArrayList<Jogador> ranking) {
+            if(ranking.size() >= 10) {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println(ranking.get(i).getNome() + " - " + (i + 1) + "º Lugar");
+                }
+            } else {
+                for (int i = 0; i < ranking.size(); i++) {
+                    System.out.println(ranking.get(i).getNome() + " - " + (i + 1) + "º Lugar");
+                }
+            }
+        }
+    }
+
