2264ec2b34550cadf87ed43895aca9357b0f5bb5
Matheus <112503237+MatthewsVieira@users.noreply.github.com>
2024-02-23 11:32:23 -0300

    Exercicio 8 - Semana 7

--- semana 7/professor dados.sql
+++ semana 7/professor dados.sql
@@ -12,3 +12,15 @@ VALUES ('Guilherme Almeida', 'IA');
 
 INSERT INTO professor (nome, especialidade)
 VALUES ('Giovanna', 'HMTL & CSS');
+
+UPDATE professor 
+  	SET id_curso = 1
+	WHERE nome = 'Lucas'
+	
+UPDATE professor 
+  	SET id_curso = 1
+	WHERE nome = 'Giovanna';
+	
+UPDATE professor 
+  	SET id_curso = 2
+	WHERE especialidade = 'Java' OR especialidade = 'Banco de Dados' OR especialidade = 'IA';
