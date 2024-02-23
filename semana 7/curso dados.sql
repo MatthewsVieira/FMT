d466f9141daa88c0b4d6d09533f2f43b16229302
Matheus <112503237+MatthewsVieira@users.noreply.github.com>
2024-02-23 10:55:54 -0300

    Exercicio 5 - Semana 7

---
+++ semana 7/curso dados.sql
@@ -0,0 +1,8 @@
+INSERT INTO curso (nome, descricao, carga_horaria)
+VALUES ('Sistemas da Informação', 'Para pessoas que gostam de tecnologia', 4000);
+
+INSERT INTO curso (nome, descricao, carga_horaria)
+VALUES ('Ciência da Computação', 'Para pessoas que gostam de tecnologia', 3000);
+
+INSERT INTO curso (nome, descricao, carga_horaria)
+VALUES ('Matématica', 'Para pessoas que gostam de números', 2000);
