fe9db1f3f46922bb07ae5645d4777ddc26920ea1
Matheus <112503237+MatthewsVieira@users.noreply.github.com>
2024-02-23 10:45:59 -0300

    Exercicio 3 - Semana 7

---
+++ semana 7/professor dados.sql
@@ -0,0 +1,14 @@
+INSERT INTO professor (nome, especialidade)
+VALUES ('Abner', 'Java');
+
+INSERT INTO professor (nome, especialidade)
+VALUES ('Marilia', 'Banco de Dados');
+
+INSERT INTO professor (nome, especialidade)
+VALUES ('Lucas', 'Java Script');
+
+INSERT INTO professor (nome, especialidade)
+VALUES ('Guilherme Almeida', 'IA');
+
+INSERT INTO professor (nome, especialidade)
+VALUES ('Giovanna', 'HMTL & CSS');
